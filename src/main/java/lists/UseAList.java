40e9d9c822f2b1f206b6471d06bcf9427c52b5d3
Simon Roberts <simon@dancingcloudservices.com>
2020-07-13 09:02:44 -0600

    Break One

---
+++ src/main/java/lists/UseAList.java
@@ -0,0 +1,34 @@
+package lists;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class UseAList {
+  public static void breakList(List l) {
+    l.add(0, LocalDate.now());
+  }
+
+  public static void main(String[] args) {
+//    List<String> names = new ArrayList<String>(
+    List<String> names = new ArrayList<>(
+        Arrays.asList(/*LocalDate.now(), */"Alice", "Bob"));
+//    names = Collections.checkedList(names, String.class);
+    names.add("Fred");
+    names.add("Jim");
+    names.add("Sheila");
+//    names.add(LocalDate.now());
+
+//    breakList(names);
+
+//    String o = (String)names.get(3);
+    String o = names.get(0); // "That Can't Happen!!"
+    System.out.println("name is " + o);
+
+    List<LocalDate> lld = new ArrayList();
+//    lld.add("Fred");
+    lld.add(LocalDate.now());
+  }
+}
